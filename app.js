75c8c19394fcddb1b207649997bad653db2967e0
VanHai1009 <tranvanhai@dtu.edu.vn>
2025-07-03 17:47:20 +0700

    app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.use(express.json());
 
 // Import routes
 const userRoutes = require('./routes/user.routes');
+const recipeRoutes = require('./routes/recipe.routes');
+app.use('/auth', userRoutes);
 app.use('/users', userRoutes);
+app.use('/recipes', recipeRoutes);
 
 module.exports = app; 
