37091e3c0caba7e8a96237355bae3afa56402d68
VanHai1009 <tranvanhai@dtu.edu.vn>
2025-07-16 00:16:29 +0700

    data

--- config/db.js
+++ config/db.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 
-
 const connectDB = async () => {
- 
   try {
-    await mongoose.connect('', {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-
     });
     console.log('Đã kết nối MongoDB!');
   } catch (err) {
@@ -16,4 +13,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB; 
+module.exports = connectDB;
